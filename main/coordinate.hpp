f9d2dba8106ecd17f76e84b19b5b39131587528c
MegatMikhail <megatmikhail03@gmail.com>
2024-06-23 17:43:24 +0800

    Add comments to coordinate.hpp

--- main/coordinate.hpp
+++ main/coordinate.hpp
@@ -1,9 +1,19 @@
+/**
+* @section DESCRIPTION
+*
+* Defines two coordinate systems (vectors and quaternions) for use in the code.
+*/
+
 #include "defines.hpp"
 #include <math.h>
 
 #ifndef VECTOR_H
 #define VECTOR_H
-// Creating a new custom data type Vector3
+
+/**
+ * @brief Create a new Vector3 data type which allows us to store 
+ pitch, yaw, and roll data values as 3-dimensional vectors. 
+ */
 class Vector3
 {
 public:
@@ -14,11 +24,19 @@ public:
     Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
     ~Vector3() {}
 
+    /**
+     * @brief Convert the data values from degrees to radians.
+     * @return Vector3 object with the values in radians.
+     */
     Vector3 toRadians()
     {
         return Vector3(x * DEG_TO_RAD, y * DEG_TO_RAD, z * DEG_TO_RAD);
     }
 
+    /**
+     * @brief Convert the data values from radians to degrees.
+     * @return Vector3 object with the values in degrees.
+     */
     Vector3 toDegrees()
     {
         return Vector3(x * RAD_TO_DEG, y * RAD_TO_DEG, z * RAD_TO_DEG);
@@ -26,9 +44,15 @@ public:
 };
 #endif
 
+
 #ifndef QUATERNION_H
 #define QUATERNION_H
 
+/**
+ * @brief Create a new Quaternion data type which allows us to store data values of
+ the craft's attitude as a quaternion (in this case represented by a 4-dimensional
+ vector).
+ */
 class Quaternion
 {
 public:
@@ -40,6 +64,10 @@ public:
     Quaternion(float w, float x, float y, float z) : w(w), x(x), y(y), z(z) {}
     ~Quaternion() {}
 
+    /**
+     * @brief Convert the data values from a quaternion to Euler angles in radians.
+     * @return Vector3 object with pitch, yaw, and roll values in radians.
+     */
     Vector3 toRadians() const
     {
         float pitch = -1 * PI / 2 + 2 * atan2(sqrt(1 + 2 * (w * y - x * z)), sqrt(1 - 2 * (w * y - x * z)));
@@ -48,6 +76,11 @@ public:
         return Vector3(pitch, roll, yaw);
     }
 
+    
+    /**
+     * @brief Convert the data values from a quaternion to Euler angles in degrees.
+     * @return Vector3 object with pitch, yaw, and roll values in degrees.
+     */
     Vector3 toDegrees() const
     {
         Vector3 angles = toRadians();
