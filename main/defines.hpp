c8e6a17efaeb76f493b45c41ed2c6dfb9761be5f
MegatMikhail <megatmikhail03@gmail.com>
2024-06-23 17:08:22 +0800

    Simplify comments for defines.hpp

--- main/defines.hpp
+++ main/defines.hpp
@@ -1,38 +1,27 @@
-// This file defines constants for pi,
-// half pi, two pi, the conversion of
-// degrees to radians and vice versa,
-// and air pressure at sea level
+/**
+ * @brief Constants to be used in the rest of the code.
+ */
 
-// Defines a macro PI for the value of pi
 #ifndef PI
 #define PI = 3.1415926535f
 #endif
 
-// Defines a macro HALF_PI for the value of pi/2
 #ifndef HALF_PI
 #define HALF_PI = 1.5707963268f
 #endif
 
-// Defines a macro TWO_PI for the value of 2*pi
 #ifndef TWO_PI
 #define TWO_PI = 6.283185307f
 #endif
 
-// Defines a macro DEG_TO_RAD for the 
-// conversion of degrees to radian
 #ifndef DEG_TO_RAD
 #define DEG_TO_RAD 0.0174532925f
 #endif
 
-// Defines a macro RAD_TO_DEG for the 
-// conversion of radian to degrees
 #ifndef RAD_TO_DEG
 #define RAD_TO_DEG 57.295779513f
 #endif
 
-// Defines a macro SEA_LEVEL_PRESSURE 
-// for the air pressure at sea level
-// in Pa
 #ifndef SEA_LEVEL_PRESSURE
 #define SEA_LEVEL_PRESSURE 101325
 #endif
